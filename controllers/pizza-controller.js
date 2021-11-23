0d6590a888dca11baa22457483d972795ddc09b6
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-11-22 21:22:33 -0800

    add pizza controller

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,67 @@
+const { Pizza } = require("../models");
+
+const pizzaController = {
+  //GET all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  //GET one pizza
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then((dbPizzaData) => {
+        //If no pizza found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id" });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  //CREATE one pizza
+  createPizza({ body }, res) {
+    console.log(body);
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.json(err));
+  },
+  //UPDATE one pizza by Id
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id" });
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  //DELETE one pizza by Id
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id" });
+        }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
+
+module.exports = pizzaController;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt");
+
+mongoose.set("debug", true);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
